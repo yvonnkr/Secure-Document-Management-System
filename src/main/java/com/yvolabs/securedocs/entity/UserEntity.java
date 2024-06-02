88e093e70daa7608d4ff17ab07cb7e83d9ec154b
yvonnkr <yvonnkr86@gmail.com>
2024-06-02 21:02:46 +0100

    add application, db, email configurations, use docker compose to spin up local postgres db & pgadmin

--- src/main/java/com/yvolabs/securedocs/entity/UserEntity.java
+++ src/main/java/com/yvolabs/securedocs/entity/UserEntity.java
@@ -48,7 +48,7 @@ public class UserEntity extends Auditable {
     @JsonIgnore
     private String qrCodeSecret;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = "text")
     private String qrCodeImageUri;
 
     @ManyToOne(fetch = FetchType.EAGER)
