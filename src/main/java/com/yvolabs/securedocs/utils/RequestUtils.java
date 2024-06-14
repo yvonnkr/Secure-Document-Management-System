14260f22d13092bd75cb1393bc9ec5365ef8d94a
yvonnkr <yvonnkr86@gmail.com>
2024-06-14 22:09:24 +0100

    implement api exception handling

--- src/main/java/com/yvolabs/securedocs/utils/RequestUtils.java
+++ src/main/java/com/yvolabs/securedocs/utils/RequestUtils.java
@@ -1,17 +1,16 @@
 package com.yvolabs.securedocs.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.yvolabs.securedocs.domain.Response;
 import com.yvolabs.securedocs.exception.ApiException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Builder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.CredentialsExpiredException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.LockedException;
+import org.springframework.security.authentication.*;
 
 import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
@@ -22,8 +21,7 @@ import java.util.function.BiFunction;
 import static java.util.Collections.emptyMap;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCauseMessage;
-import static org.springframework.http.HttpStatus.FORBIDDEN;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.*;
 
 /**
  * @author Yvonne N
@@ -77,7 +75,31 @@ public class RequestUtils {
         if (exception instanceof AccessDeniedException) {
             Response apiResponse = getErrorResponse(request, response, exception, FORBIDDEN);
             writeResponse.accept(response, apiResponse);
+        } else if (exception instanceof InsufficientAuthenticationException) {
+            Response apiResponse = getErrorResponse(request, response, exception, UNAUTHORIZED);
+            writeResponse.accept(response, apiResponse);
+        } else if (exception instanceof MismatchedInputException) {
+            Response apiResponse = getErrorResponse(request, response, exception, BAD_REQUEST);
+            writeResponse.accept(response, apiResponse);
+        } else if (exception instanceof DisabledException || exception instanceof LockedException || exception instanceof BadCredentialsException || exception instanceof CredentialsExpiredException || exception instanceof ApiException) {
+            Response apiResponse = getErrorResponse(request, response, exception, BAD_REQUEST);
+            writeResponse.accept(response, apiResponse);
+        } else {
+            Response apiResponse = getErrorResponse(request, response, exception, INTERNAL_SERVER_ERROR);
+            writeResponse.accept(response, apiResponse);
         }
+    }
+
+    public static Response handleErrorResponse(String message, String exception, HttpServletRequest request, HttpStatusCode status) {
+        return Response.builder()
+                .time(LocalDateTime.now().toString())
+                .code(status.value())
+                .path(request.getRequestURI())
+                .status(HttpStatus.valueOf(status.value()))
+                .message(message)
+                .exception(exception)
+                .data(emptyMap())
+                .build();
     }
 
     private static Response getErrorResponse(HttpServletRequest request, HttpServletResponse response, Exception exception, HttpStatus status) {
